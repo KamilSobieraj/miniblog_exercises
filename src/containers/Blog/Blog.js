daae61ce784ec2ecee5b9379c59d5fc1441f8606
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-06-05 13:14:35 +0200

    Selecting single post to display

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,7 +7,8 @@ import "./Blog.css";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
@@ -18,15 +19,23 @@ class Blog extends Component {
       this.setState({ posts: updatedPosts });
     });
   }
+  postSelectedHandler(id) {
+    this.setState({ selectedPostId: id });
+  }
   render() {
     const posts = this.state.posts.map(post => (
-      <Post key={post.id} title={post.title} author={post.author} />
+      <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        clicked={() => this.postSelectedHandler(post.id)}
+      />
     ));
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
