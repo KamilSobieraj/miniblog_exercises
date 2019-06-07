2c2270f6048db009cfe6638721b50ed322748427
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-06-07 11:59:58 +0200

    Pre-routing folder restructure

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,39 +1,13 @@
 import React, { Component } from "react";
-import axios from "axios";
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
+// import axios from "axios";
+// import Post from "../../components/Post/Post";
+// import FullPost from "./FullPost/FullPost";
+// import NewPost from "./NewPost/NewPost";
+import Posts from "./Posts/Posts";
 import "./Blog.css";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null
-  };
-  componentDidMount() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map(post => {
-          return { ...post, author: "Jaśmin" };
-        });
-        this.setState({ posts: updatedPosts });
-      })
-      .catch(error => console.log(error));
-  }
-  postSelectedHandler(id) {
-    this.setState({ selectedPostId: id });
-  }
   render() {
-    const posts = this.state.posts.map(post => (
-      <Post
-        key={post.id}
-        title={post.title}
-        author={post.author}
-        clicked={() => this.postSelectedHandler(post.id)}
-      />
-    ));
     return (
       <div className="Blog">
         <header>
@@ -48,13 +22,13 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">{posts}</section>
-        <section>
+        <Posts />
+        {/* <section>
           <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
-        </section>
+        </section> */}
       </div>
     );
   }
